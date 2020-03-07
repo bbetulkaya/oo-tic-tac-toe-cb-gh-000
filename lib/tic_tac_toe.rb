72891bf0a385acf8090b9cafdea69975f56a0f5e
Betul Kaya <betulkaya23@hotmail.com>
2020-03-07 20:15:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,24 +1,26 @@
 class TicTacToe
-  def initialize(board)
-    @board = Array.new(9," ")
-  WIN_COMBINATIONS=[
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [6,4,2]
-          ]
-          def display_board
-            puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-            puts "-----------"
-            puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-            puts "-----------"
-            puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-          end
 
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
+def initialize
+  @board = Array.new(9, " ")
+end
+
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 
 def input_to_index(user_input)
@@ -135,4 +137,5 @@ def play
     puts "Cat's Game!"
   end
 end
+
 end
